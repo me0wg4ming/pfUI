a779500f8f747ff8d008a7bfdcf2af1314c0c828
Jrc13245 <grc2.grc2@gmail.com>
2026-01-01 17:27:42 -0500

    fix pusback in cast timers

--- libs/libcast.lua
+++ libs/libcast.lua
@@ -214,7 +214,7 @@ libcast:SetScript("OnEvent", function()
     end
   elseif event == "SPELLCAST_DELAYED" then
     if this.db[player].cast then
-      this.db[player].start = this.db[player].start + arg1/1000
+      this.db[player].casttime = this.db[player].casttime + arg1
     end
   elseif event == "SPELLCAST_CHANNEL_START" then
     -- add cast action to the database
