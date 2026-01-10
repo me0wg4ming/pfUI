9fc161df217dd383ea2f379128015eba5ecaeed6
Meow <30401521+me0wg4ming@users.noreply.github.com>
2026-01-10 13:34:40 +0100

    forgot raidframe throttle
    Added additionally a raidframe roster throttle

--- modules/raid.lua
+++ modules/raid.lua
@@ -87,6 +87,10 @@ pfUI:RegisterModule("raid", "vanilla:tbc", function ()
   pfUI.uf.raid:RegisterEvent("VARIABLES_LOADED")
   pfUI.uf.raid:SetScript("OnEvent", function() this:Show() end)
   pfUI.uf.raid:SetScript("OnUpdate", function()
+    -- Throttle raid roster updates to 5 FPS
+    if (this.tick or 0) > GetTime() then return end
+    this.tick = GetTime() + 0.2
+
     -- don't proceed without raid or during combat
     if not UnitInRaid("player") or (InCombatLockdown and InCombatLockdown()) then return end
 
