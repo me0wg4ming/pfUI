727bc5dee776094f889c3209e85d220fca48e20b
Meow <30401521+me0wg4ming@users.noreply.github.com>
2026-03-14 17:11:47 +0100

    small fix for mana ticks still showing in some situations.

--- modules/energytick.lua
+++ modules/energytick.lua
@@ -66,6 +66,7 @@ pfUI:RegisterModule("energytick", "vanilla:tbc", function()
       elseif this.mode == "MANA" and diff > 0 then
         if UnitMana("player") >= UnitManaMax("player") then
           this.start = nil
+          this.spark:SetAlpha(0)
           this:Hide()
         elseif this.max ~= 5 and diff > (this.badtick and this.badtick * 1.2 or 5) then
           this.target = 2
@@ -103,6 +104,12 @@ pfUI:RegisterModule("energytick", "vanilla:tbc", function()
     this.current = GetTime() - this.start
 
     if this.current > this.max then
+      -- Don't restart tick timer if mana is full
+      if this.mode == "MANA" and UnitMana("player") >= UnitManaMax("player") then
+        this.start = nil
+        this.spark:SetAlpha(0)
+        return
+      end
       this.start, this.max, this.current = GetTime(), getAdjustedTickTimer(), 0
     end
 
