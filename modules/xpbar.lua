49fd015d2038c30364770278008d2e0e35a8b212
Meow <30401521+me0wg4ming@users.noreply.github.com>
2026-03-27 14:35:48 +0100

    turtle-wow change
    - XP bar for rested xp has been capped to 100% max  by turtle-wow, not 150% - adjusted the formular to be accurate.

--- modules/xpbar.lua
+++ modules/xpbar.lua
@@ -45,7 +45,7 @@ local function OnEnter(self)
     local xp_perc = round(xp / xpmax * 100)
     local remaining = xpmax - xp
     local remaining_perc = round(remaining / xpmax * 100)
-    local exh_perc = GetXPExhaustion() and round(GetXPExhaustion() / xpmax / 0.75 * 100) or 0
+    local exh_perc = GetXPExhaustion() and round(GetXPExhaustion() / xpmax / 1.125 * 100) or 0
     local xp_persec = ((xp - data.startxp)/(GetTime() - data.starttime))
     local session = UnitXP("player") - data.startxp
     local avg_hour = floor(((UnitXP("player") - data.startxp) / (GetTime() - data.starttime)) * 60 * 60)
@@ -171,7 +171,7 @@ end
       local text = "%s: %s%%"
       local xp, xpmax, ex = UnitXP("player"), UnitXPMax("player"), GetXPExhaustion()
       local xpperc = round(xp / xpmax * 100)
-      local experc = ex and round(ex / xpmax / 0.75 * 100) or 0
+      local experc = ex and round(ex / xpmax / 1.125 * 100) or 0
       if ex then text = "%s: %s%% (%s%% %s)" end
       self.bar.text:SetText(string.format(text, T["Experience"], xpperc, experc, T["Rested"]))
 
